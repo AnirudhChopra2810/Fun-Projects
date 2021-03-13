dcc85158aa835c388685b2e59c4aee94573748c8
AnirudhChopra2810 <65032965+AnirudhChopra2810@users.noreply.github.com>
2021-03-14 04:30:27 +0530

    some changes added

--- src/Components/amazonPirime.js
+++ src/Components/amazonPirime.js
@@ -1,18 +1,18 @@
 import { React, useEffect, useState } from "react";
 import axios from "axios";
 import "./styles.css";
-import image from "./netflix.png";
+import image from "./prime.png";
 
-const Netflix = () => {
-  const [netflixList, setNetflixList] = useState([]);
+const Prime = () => {
+  const [primeList, setPrimeList] = useState([]);
 
   useEffect(async () => {
     const API_KEY = "AIzaSyDS-Ahrwz6gA4WOf-y6CVl_7B3TO98DrqA";
-    const channelId = "UCWOA1ZGywLbqmigxE4Qlvuw";
+    const channelId = "UC4zWG9LccdWGUlF77LZ8toA";
     const maxResult = 10;
     const url = `https://youtube.googleapis.com/youtube/v3/search?maxResults=${maxResult}&q=Trailer&channelId=${channelId}&key=${API_KEY}`;
 
-    const netflixApi = await axios
+    const primeApi = await axios
       .get(url)
       .then((response) => {
         let data = response.data;
@@ -20,7 +20,7 @@ const Netflix = () => {
         const link = data.items.map(
           (obj) => "https://www.youtube.com/embed/" + obj.id.videoId
         );
-        setNetflixList(link);
+        setPrimeList(link);
       })
       .catch((error) => console.log(error));
   }, []);
@@ -28,10 +28,10 @@ const Netflix = () => {
   return (
     <div>
       <div>
-        <img src={image} className="title"></img>
+        <img src={image} className="primetitle"></img>
       </div>
-      <div className="Container">
-        {netflixList.map((link, id) => {
+      <div className="PrimeContainer">
+        {primeList.map((link, id) => {
           return (
             <iframe
               className="frame"
@@ -49,4 +49,4 @@ const Netflix = () => {
   );
 };
 
-export default Netflix;
+export default Prime;
--- src/Components/app.js
+++ src/Components/app.js
@@ -1,10 +1,12 @@
 import { React, useState } from "react";
 import Netflix from "./netflix";
+import Prime from "./amazonPirime";
 
 const App = () => {
   return (
     <div>
       <Netflix />
+      <Prime />
     </div>
   );
 };
